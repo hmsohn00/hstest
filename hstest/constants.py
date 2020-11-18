29d9684c3d34c87c61992221c5df2c6a9c46ca9d
unknown <hmsohn00@gmail.com>
2020-11-18 17:06:48 +0100

    update

---
+++ hstest/constants.py
@@ -0,0 +1,7 @@
+import os
+
+__version__ = "0.1"
+
+package_path = os.path.realpath(os.path.join(os.path.realpath(__file__), os.pardir))
+package_name = os.path.basename(package_path)
+schema_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "settings-schema.json")
